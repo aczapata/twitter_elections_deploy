c9decad2b0a66e2059141586dd234a8285f0e2f1
aczapata <andreaczapata@gmail.com>
2016-05-18 14:29:51 -0500

    Change static dir

--- twitter/settings.py
+++ twitter/settings.py
@@ -5,13 +5,13 @@ import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
---
+++ twitter/static/collector/js/init.11c1764e9760.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+    $('select').material_select();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
